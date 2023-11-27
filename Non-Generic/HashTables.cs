23660a2a8802d8db6e17ce22ac19626492bcc7d4
samuelkamotho92 <samuelkamotho92@gmail.com>
2023-11-28 00:18:20 +0300

    Check Most Appearing

---
+++ Non-Generic/HashTables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+using System.Xml;
+namespace Collections.Non_Generic
+{
+
+  /*  The Keys can be of any data type but they must be unique and not null.
+The Hashtable accepts both null and duplicate values.
+We can access the values by using the associated key.
+The capacity of a Hashtable is the number of elements that a Hashtable can hold.
+A hashtable is a non-generic collection, so we can store elements of the same type as well as of different types.*/
+
+    public class HashTables
+    {
+
+    }
+}
